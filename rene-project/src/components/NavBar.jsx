1c40f6f5e18dbffa057a7cfd5ed115da9058a5d7
Henry York <hay1g21@soton.ac.uk>
2025-01-06 00:27:34 +0000

    added NavBar Component

---
+++ rene-project/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+
+
+
+function NavBar (){
+    return(
+    <>
+        <div className='navbar'>
+            <ul>
+                <li><a href ="#home">Home</a></li>
+                <li><a href ="#profile">René Magritte</a></li>
+                <li><a href ="#favourites">Author's Favourites</a></li>
+                <li><a href ="#quiz">Quiz</a></li>
+                <li><a href ="#draw">Draw Surreal</a></li>
+            </ul>
+        </div>    
+    </>
+    )
+}
+
+export default NavBar;
