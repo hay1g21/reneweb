dc75d547deabde179c22f27682a9931b722be892
Henry York <hay1g21@soton.ac.uk>
2025-01-13 00:54:35 +0000

    expand topbar animation

--- rene-project/src/components/NavBar.jsx
+++ rene-project/src/components/NavBar.jsx
@@ -1,17 +1,21 @@
-
+import { useState } from "react";
 
 
 function NavBar (){
+    const [expand, setExpand] = useState(false);
+    
     return(
     <>
         <nav className='navbar'>
             <ul>
-                <li><a href ="#home">Home</a></li>
-                <li><a href ="#profile">René Magritte</a></li>
-                <li><a href ="#favourites">Author's Favourites</a></li>
-                <li><a href ="#quiz">Quiz</a></li>
-                <li><a href ="#draw">Draw Surreal</a></li>
+                <li><a href ="#home" className={expand ? "navAnim" : "navAnimRev"}>Home</a></li>
+                <li><a href ="#profile" className={expand ? "navAnim" : "navAnimRev"}>René Magritte</a></li>
+                <li><a href ="#favourites" className={expand ? "navAnim" : "navAnimRev"}>Author's Favourites</a></li>
+                <li><a href ="#quiz" className={expand ? "navAnim" : "navAnimRev"}>Quiz</a></li>
+                <li><a href ="#draw" className={expand ? "navAnim" : "navAnimRev"}>Draw Surreal</a></li>
+                <button onClick={() => expand ? setExpand(false) : setExpand(true)}>CLICK HERE</button>
             </ul>
+           
         </nav>    
     </>
     )
